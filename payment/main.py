929c8832e8f1ea139e139af540d0201b4a95b3e6
Abhay Singh Rana <abhay.rana@infoorigin.com>
2026-02-22 17:26:48 +0530

    first commit

---
+++ payment/main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
+import time
+
+app = FastAPI()
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['http://localhost:3000'],
+        allow_methods=['*'],
+        allow_headers=['*'],
+)
+
+
+#Should be a different database
+redis = get_redis_connection(
+        host="redis-19421.crce276.ap-south-1-3.ec2.cloud.redislabs.com",
+        port=19421,
+        password="C2umEUC6eTosUli4SWDQWe6lSlf80WPn",        
+)
+
+
+class Order(HashModel):
+    product_id: str 
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
+
+    class Meta:
+        database = redis
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk)
+
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks):
+
+    body = await request.json()
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+            product_id = product['pk'],
+            price = product['price'],
+            fee = 0.2 * product['price'],
+            total = 1.2 * product['price'],
+            quantity = body['quantity'],
+            status = 'pending'
+    )
+    order.save()
+    background_tasks.add_task(order_completed, order)
+
+    return order
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+
+    order.save()
